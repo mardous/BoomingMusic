041c0d16faca803df1e3d28203b21555112ed07b
Christian <mardous.contact@gmail.com>
2025-12-21 17:08:08 -0400

    feat(ui): replace Manrope font with Google Sans Flex
    Signed-off-by: Christian <mardous.contact@gmail.com>

--- app/src/main/java/com/mardous/booming/ui/component/base/AbsThemeActivity.kt
+++ app/src/main/java/com/mardous/booming/ui/component/base/AbsThemeActivity.kt
@@ -75,7 +75,7 @@ abstract class AbsThemeActivity : AppCompatActivity() {
             )
         }
         if (Preferences.isCustomFont) {
-            setTheme(R.style.ManropeThemeOverlay)
+            setTheme(R.style.CustomFontThemeOverlay)
         }
     }
 
