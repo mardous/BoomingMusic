11c757cb17c51fc57793d0eb40b9ceca62f9720e
TheTerminatorOfProgramming <41417119+TheTerminatorOfProgramming@users.noreply.github.com>
2026-01-11 00:17:20 +0800

    Set as Ringtone Dialog Compose
    Convert the Set as Ringtone Dialog to Compose

---
+++ app/src/main/java/com/mardous/booming/ui/screen/ringtone/RingtoneFragment.kt
@@ -0,0 +1,56 @@
+package com.mardous.booming.ui.screen.ringtone
+
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mardous.booming.R
+import com.mardous.booming.data.model.Song
+import com.mardous.booming.extensions.dip
+import com.mardous.booming.ui.theme.BoomingMusicTheme
+import kotlin.getValue
+
+class RingtoneFragment: BottomSheetDialogFragment() {
+    private val navArgs: RingtoneFragmentArgs by navArgs()
+
+    private val song: Song
+        get() = navArgs.extraSong
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog?.setOnShowListener { dialogInterface ->
+            val bottomSheetDialog = dialogInterface as BottomSheetDialog
+            bottomSheetDialog.behavior.apply {
+                isFitToContents = true
+                skipCollapsed = true
+                peekHeight = dip(R.dimen.shuffle_height)
+                //isDraggable = false
+                maxHeight = peekHeight
+            }
+        }
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                BoomingMusicTheme {
+                    if (!Settings.System.canWrite(requireContext())) {
+                        RingtonePermissionBottomSheet(
+                            song = song
+                        )
+                    } else {
+                        RingtoneBottomSheet(
+                            song = song
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
