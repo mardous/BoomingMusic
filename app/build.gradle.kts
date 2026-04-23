9b4b5951926fd79d14c775b6baa320ecc9e02031
Christian <mardous.contact@gmail.com>
2026-04-23 15:11:40 -0400

    build: update version to 1.3.0-beta.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ val currentVersion: Version = Version.Beta(
     versionMajor = 1,
     versionMinor = 3,
     versionPatch = 0,
-    versionBuild = 3
+    versionBuild = 4
 )
 val currentVersionCode = currentVersion.code
 
@@ -87,7 +87,7 @@ android {
         targetSdk = 36
 
         applicationId = namespace
-        versionCode = 1300103
+        versionCode = 1300104
         versionName = currentVersion.name
         check(versionCode == currentVersionCode)
     }
