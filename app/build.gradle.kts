3f4a0509dff4266365b0e39cb7ef0d61bc7b8b80
Christian <mardous.contact@gmail.com>
2026-06-13 20:08:56 -0400

    build: bump version to 1.3.1-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ val currentVersion: Version = Version.Beta(
     versionMajor = 1,
     versionMinor = 3,
     versionPatch = 1,
-    versionBuild = 1
+    versionBuild = 2
 )
 val currentVersionCode = currentVersion.code
 
@@ -87,7 +87,7 @@ android {
         targetSdk = 36
 
         applicationId = namespace
-        versionCode = 1310101
+        versionCode = 1310102
         versionName = currentVersion.name
         check(versionCode == currentVersionCode)
     }
